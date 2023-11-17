979ee89388c7a934d81107c8a9425fc10e2a9940
Mahesh <102217496+Mahesh-1919@users.noreply.github.com>
2023-11-17 12:45:32 +0530

    Update skills.jsx

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -5,17 +5,17 @@ function skills(){
             <section className="skills" id="skills">
                 <h1>Skills</h1>
                 <ul className="skill-container">
-                    <li><img src="/src/assets/image1.png" alt="" /></li>
-                    <li><img src="/src/assets/image2.png" alt="" /></li>
-                    <li><img src="/src/assets/image3.png" alt="" /></li>
-                    <li><img src="/src/assets/image4.png" alt="" /></li>
-                    <li><img src="/src/assets/image5.png" alt="" /></li>
-                    <li><img src="/src/assets/image6.png" alt="" /></li>
-                    <li><img src="/src/assets/image7.png" alt="" /></li>
-                    <li><img src="/src/assets/image8.png" alt="" /></li>
-                    <li><img src="/src/assets/image9.png" alt="" /></li>
-                    <li><img src="/src/assets/image10.png" alt="" /></li>
-                    <li><img src="/src/assets/image11.png" alt="" /></li>
+                    <li><img src="/assets/image1.png" alt="" /></li>
+                    <li><img src="/assets/image2.png" alt="" /></li>
+                    <li><img src="/assets/image3.png" alt="" /></li>
+                    <li><img src="/assets/image4.png" alt="" /></li>
+                    <li><img src="/assets/image5.png" alt="" /></li>
+                    <li><img src="/assets/image6.png" alt="" /></li>
+                    <li><img src="/assets/image7.png" alt="" /></li>
+                    <li><img src="/assets/image8.png" alt="" /></li>
+                    <li><img src="/assets/image9.png" alt="" /></li>
+                    <li><img src="/assets/image10.png" alt="" /></li>
+                    <li><img src="/assets/image11.png" alt="" /></li>
                   
                 </ul>
             </section>
